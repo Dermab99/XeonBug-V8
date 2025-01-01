26f5ffe4edd1f924b14ac484018c396d006fef8e
Dermab99 <bosox9900@gmail.com>
2025-01-01 08:23:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "628887172714"
+global.ownernomer = "6285731539307"
 global.ownername = "Qasim Ali"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '628887172714'  //creator number
+global.ownernumber = '6285731539307'  //creator number
 global.ownername = 'Qasim Ali' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
